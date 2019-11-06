2f4be816adc26d70f17616dcedcf4dc764bfc7e5
Guilherme Martin <gsilvamartin@gmail.com>
2019-11-06 20:37:34 -0300

    Configuração repository-service

--- src/socket/Main.js
+++ src/socket/Main.js
@@ -1,12 +1,6 @@
 const express = require('express')
 const server = express()
-const dbConnection = require('../database/DatabaseConnection')
-let dbClient = null;
 
 server.listen(3000, async () =>{
-    dbClient = await dbConnection.connectDatabase();
 
-    if(dbClient != null){
-        console.log("Aplicação e banco de dados iniciados com sucesso!")
-    }
 });
