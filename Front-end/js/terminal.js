604872ba911a14049443202158ebbe9af11c7f68
Guilherme Martin <gsilvamartin@gmail.com>
2019-12-06 13:26:28 -0300

    remove xtermjs

--- Front-end/js/terminal.js
+++ Front-end/js/terminal.js
@@ -1,44 +1,10 @@
-const termJS = new Terminal({ cursorBlink: true });
-
 /**
  * Initialize terminal.
  *
  * @author Guilherme da Silva Martin
  */
 function initTerminal() {
-  termJS.open(document.getElementById('terminal'));
-  termJS.writeln('Welcome to RTCode');
-  termJS.writeln('Connecting to server...');
 
-  termJS.onKey((e) => {
-    const printable = !e.domEvent.altKey && !e.domEvent.altGraphKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
-
-    if (e.domEvent.keyCode === 13) {
-      termJS.write('\r\n$ ');
-    } else if (e.domEvent.keyCode === 8) {
-      if (termJS._core.buffer.x > 2) {
-        termJS.write('\b \b');
-      }
-    } else if (printable) {
-      termJS.write(e.key);
-    }
-  });
-
-  socketTerminal.on('connect', () => {
-    termJS.write('\r\n*** Connected to server***\r\n');
-
-    termJS.on('data', (data) => {
-      socketTerminal.emit('data', data);
-    });
-
-    socketTerminal.on('data', (data) => {
-      termJS.write(data);
-    });
-
-    socketTerminal.on('disconnect', () => {
-      termJS.write('\r\n*** Disconnected from server***\r\n');
-    });
-  });
 }
 
 /**
@@ -47,5 +13,5 @@ function initTerminal() {
  * @author Guilherme da Silva Martin
  */
 $(document).ready(() => {
-  initTerminal();
+    initTerminal();
 });
