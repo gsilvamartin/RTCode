b43bb8e7b5031615bf71e0263252f947ceab208f
Guilherme Martin <gsilvamartin@gmail.com>
2019-12-09 13:20:32 -0300

    TermJS handle input

--- Front-end/js/terminal.js
+++ Front-end/js/terminal.js
@@ -1,11 +1,30 @@
+const term = new Terminal();
+
 /**
  * Initialize terminal.
  *
  * @author Guilherme da Silva Martin
  */
 function initTerminal() {
+    term.open(document.getElementById('terminal'));
 
+    term.write('Hello World \r\n');
+    term.write('~$ ');
 }
+
+/**
+ * Handle user key input.
+ * 
+ * @author Guilherme da Silva Martin
+ */
+term.onKey((data) => {
+    if (data.domEvent.key === 'Enter') {
+        term.write('\r\n');
+        term.write('~$ ');
+    } else {
+        term.write(data.key);
+    }
+});
 
 /**
  * Runs as soon as the page is ready.
