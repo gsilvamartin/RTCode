5cb68b9c98f815e9ff0783f66bd97a033666a9ed
Guilherme Martin <gsilvamartin@gmail.com>
2019-12-14 17:54:10 -0300

    context menu jstree

--- Front-end/js/file-tree.js
+++ Front-end/js/file-tree.js
@@ -21,6 +21,58 @@ $(document).ready(() => {
         }
       ]
     },
-    plugins: ['contextmenu']
+    plugins: ['contextmenu'],
+    contextmenu: {
+      items: function($node) {
+        let tree = $('#file-tree').jstree(true);
+
+        return {
+          Create: {
+            separator_before: false,
+            separator_after: true,
+            label: 'Create',
+            action: false,
+            submenu: {
+              File: {
+                seperator_before: false,
+                seperator_after: false,
+                label: 'File',
+                action: function(obj) {
+                  $node = tree.create_node($node, { text: 'New File', type: 'file', icon: 'glyphicon glyphicon-file' });
+                  tree.deselect_all();
+                  tree.select_node($node);
+                }
+              },
+              Folder: {
+                seperator_before: false,
+                seperator_after: false,
+                label: 'Folder',
+                action: function(obj) {
+                  $node = tree.create_node($node, { text: 'New Folder', type: 'default' });
+                  tree.deselect_all();
+                  tree.select_node($node);
+                }
+              }
+            }
+          },
+          Rename: {
+            separator_before: false,
+            separator_after: false,
+            label: 'Rename',
+            action: function(obj) {
+              tree.edit($node);
+            }
+          },
+          Remove: {
+            separator_before: false,
+            separator_after: false,
+            label: 'Remove',
+            action: function(obj) {
+              tree.delete_node($node);
+            }
+          }
+        };
+      }
+    }
   });
 });
