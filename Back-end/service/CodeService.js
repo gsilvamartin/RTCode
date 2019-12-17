28eed8af7768ed8de61db7351bbce875d824f430
Guilherme Martin <gsilvamartin@gmail.com>
2019-12-17 20:39:36 -0300

    verify if code already exists

--- Back-end/service/CodeService.js
+++ Back-end/service/CodeService.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const CodeModel = require('../database/model/Code');
+const ErrorResponse = require('../model/response/ErrorResponse');
 const GenericRepository = require('../database/repository/GenericRepository');
 
 module.exports = class CodeService {
@@ -22,13 +23,17 @@ module.exports = class CodeService {
    */
   static async createCode(codeName, userId) {
     try {
-      let code = {
-        CodeName: codeName,
-        IsActive: true,
-        UserId: new mongoose.Types.ObjectId(userId)
-      };
+      if ((await this.repository.count(CodeModel, { CodeName: codeName })) === 0) {
+        let code = {
+          CodeName: codeName,
+          IsActive: true,
+          UserId: new mongoose.Types.ObjectId(userId)
+        };
 
-      return this.repository.create(CodeModel, code);
+        return await this.repository.create(CodeModel, code);
+      } else {
+        throw new ErrorResponse(401, 'Code already exists', null);
+      }
     } catch (ex) {
       throw ex;
     }
